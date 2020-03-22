e6367cc1b1ccc8e2576a7cdadf41261ad30e331b
LusioF <lusiof@gmail.com>
2020-03-22 13:33:36 -0400

    tally workout data

--- public/workout.js
+++ public/workout.js
@@ -18,4 +18,27 @@ async function initWorkout() {
       renderNoWorkoutText()
     }
   }
+
+  function tallyExercises(exercises) {
+    const tallied = exercises.reduce((acc, curr) => {
+      if (curr.type === "resistance") {
+        acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
+        acc.totalSets = (acc.totalSets || 0) + curr.sets;
+        acc.totalReps = (acc.totalReps || 0) + curr.reps;
+      } else if (curr.type === "cardio") {
+        acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
+      }
+      return acc;
+    }, {});
+    return tallied;
+  }
   
+  function formatDate(date) {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    };
+
+    
