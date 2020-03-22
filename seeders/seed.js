054f4e06e64c0caba4b024d9675ab9aa3b293a53
LusioF <lusiof@gmail.com>
2020-03-22 13:44:03 -0400

    select db, set exercises

---
+++ seeders/seed.js
@@ -0,0 +1,36 @@
+let mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+let workoutSeed = [
+  {
+    day: new Date().setDate(new Date().getDate()-10),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bicep Curl",
+        duration: 20,
+        weight: 100,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-9),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Lateral Pull",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  
