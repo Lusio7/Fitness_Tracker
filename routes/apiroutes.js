f9b65dbd97160aae2b6cd5ece0f4e7c51fd516c8
LusioF <lusiof@gmail.com>
2020-03-22 13:39:21 -0400

    router put and router get methods

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -12,4 +12,31 @@ router.post("/api/workouts", ( req, res) => {
       });
   });
 
-  
+  router.put("/api/workouts/:id", ({ body, params}, res)=> {
+    Workout.findByIdAndUpdate(
+        params.id,
+        {$push: {exercises: body}
+
+        },
+        {
+            new: true, runValidators: true
+        }
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err=> {
+        res.json(err)
+    })
+})
+
+router.get ("/api/workouts", (req,res) => {
+    Workout.find()
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
