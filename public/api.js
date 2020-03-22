3ba82fa854193f59c8ff66e741c4eb7e76addc77
LusioF <lusiof@gmail.com>
2020-03-22 13:16:33 -0400

    api path

---
+++ public/api.js
@@ -0,0 +1,14 @@
+const API = {
+    async getLastWorkout() {
+      let res;
+      try {
+        res = await fetch("/api/workouts");
+      } catch (err) {
+        console.log(err)
+      }
+      const json = await res.json();
+  
+      return json[json.length - 1];
+    },
+
+    
